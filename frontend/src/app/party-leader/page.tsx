8c468dca2d05baaaf7836c1783d34176e8f969b9
Joni Niemelä <j.niemela@outlook.com>
2026-01-26 17:40:47 +0200

    cleaned unneccessary pages

--- frontend/src/app/party-leader/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const PartyLeaderRoute = () => {
-  return <div>PartyLeaderRoute</div>
-}
-
-export default PartyLeaderRoute
--- frontend/src/app/party-vote/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const PartyVoteRoute = () => {
-  return <div>PartyVoteRoute</div>
-}
-
-export default PartyVoteRoute
